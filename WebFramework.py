86510a4f1bfd006351d296d28628ec46239a3095
Eli <elibrunner2@gmail.com>
2024-02-21 10:26:16 -0800

    figuirng out api's

--- WebFramework.py
+++ WebFramework.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
