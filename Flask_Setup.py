aef2d08fa9f90795f95dda7440b530ef8dc5ac88
EliBrunner2 <elibrunner2@gmail.com>
2024-03-08 15:23:31 -0800

    fixed page bug, and fixing search bar

--- Flask_Setup.py
+++ Flask_Setup.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
-from API_Call import html_output
+from flask import Flask, request, render_template, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from API_Call import CollegeSchoolFetcher
+import requests
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 
 app = Flask(__name__)
 
@@ -7,24 +11,26 @@ app = Flask(__name__)
 def index():
     return render_template('Main_Page.html')
 
-@app.route('/college_search')
-def college_search():
-    # Render the college_search.html page
-    return render_template('college_search.html', html_output)
+college_search_bp = Blueprint("college_search", __name__)
 
-@app.route("/search")
+@college_search_bp.route('/college_search.html')
+def college_search_page():
+    # Render the college_search.html page
+    return render_template('college_search.html')
+
+@college_search_bp.route("/search", methods=["GET", "POST"])
 def search():
     q = request.args.get("q")
     print(q)
-
+    # q = query user will search from "q" and it will be searched by the name of college, price, state, etc.
     if q:
-        results = Song.query.filter(Song.title.icontains(q) | Song.performer.icontains(q)) \
-        .order_by(Song.peak_position.asc()).order_by(Song.chart_debut.desc()).limit(100).all()
+        results = CollegeSchoolFetcher.query.filter(CollegeSchoolFetcher.school_name.icontains(q) | CollegeSchoolFetcher.school_state.icontains(q)) \
+            .order_by(CollegeSchoolFetcher.instate_tuition.asc()).order_by(CollegeSchoolFetcher.outstate_tuition.asc()).limit(100).all()
     else:
         results = []
-
     return render_template("search_results.html", results=results)
 
+app.register_blueprint(college_search_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
