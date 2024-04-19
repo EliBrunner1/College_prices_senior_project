d63b6b23bdca2ee807bd0c88e8ea2f60160771a4
Eli <elibrunner2@gmail.com>
2024-04-19 13:31:54 -0700

    started working on tests

--- test.py
+++
@@ -1,8 +0,0 @@
-def get_search_url(keywords):
-  search_engine = "https://www.google.com/search?q="
-  return search_engine + keywords.replace(" ", "+")
-
-
-keywords = "Oregon state university"
-search_url = get_search_url(keywords)
-print(search_url)
